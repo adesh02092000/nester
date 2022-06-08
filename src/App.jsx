5e2c8b9f2a94228f2432d357b6b46abb52a56adc
Adesh Gupta <adesh02092000@gmail.com>
2022-06-08 15:03:04 +0530

    Logged the currently logged in user name in the profile page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
 import ForgotPassword from './pages/ForgotPassword'
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
           {/* Profile will be a protected route to be added later */}
-          <Route path='/profile' element={<SignIn />} />
+          <Route path='/profile' element={<Profile />} />
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,13 @@
+import { getAuth } from 'firebase/auth'
+import { useEffect, useState } from 'react'
+
 export default function Profile() {
-  return <div>Profile</div>
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth()
+  useEffect(() => {
+    setUser(auth.currentUser)
+  }, [])
+
+  return user ? <h1>{user.displayName}</h1> : 'Please Sign in'
 }
