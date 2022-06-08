5ef5f261dca2cda026aab0b853e029e8779e85e9
Adesh Gupta <adesh02092000@gmail.com>
2022-06-09 00:44:12 +0530

    Added Layout for forgot password

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,3 +1,46 @@
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import { Link } from 'react-router-dom'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
+import { useState } from 'react'
+
 export default function ForgotPassword() {
-  return <div>Forgot Password</div>
+  const [email, setEmail] = useState('')
+
+  const onSubmit = e => {
+    e.preventDefault()
+  }
+
+  const onChange = e => {
+    setEmail(e.target.value)
+  }
+
+  return (
+    <div className='pageContainer'>
+      <header>
+        <p className='pageHeader'>Forgot Password</p>
+      </header>
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type='email'
+            className='emailInput'
+            value={email}
+            placeholder='Email'
+            id='email'
+            onChange={onChange}
+          />
+          <Link className='forgotPasswordLink' to='/sign-in'>
+            Sign In Instead
+          </Link>
+
+          <div className='signInBar'>
+            <div className='signInText'>Send Reset Link</div>
+            <button className='signInButton'>
+              <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+      </main>
+    </div>
+  )
 }
