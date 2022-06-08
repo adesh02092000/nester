6f6a686f96afd3784c2e5dad0e8ae4f1c3093e68
Adesh Gupta <adesh02092000@gmail.com>
2022-06-08 19:24:40 +0530

    Added spinner

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
 
 export const PrivateRoute = () => {
   const { loggedIn, loading } = useAuthStatus()
 
   if (loading) {
-    return <h3>Loading...</h3>
+    return <Spinner />
   }
   // Outlet is used to render child components, so if the user is loggedIn we render the child
   // components otherwise we navigate to the sign-in page
