752b02024df2142e7d735e68cd2634fc298b7819
Adesh Gupta <adesh02092000@gmail.com>
2022-06-09 01:31:46 +0530

    Added google OAuth sign in and added new users to the database

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,12 +1,39 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import googleIcon from '../assets/svg/googleIcon.svg'
+import { db } from '../firebase.config'
 
 export default function OAuth() {
   const navigate = useNavigate()
   const location = useLocation()
 
-  const onGoogleClick = async () => {}
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user
+
+      // Save the user in database
+      const userRef = doc(db, 'users', user.uid)
+      // read the user
+      const userSnap = await getDoc(userRef)
+      // if the user does not exist in the users collection create one
+      if (!userSnap.exists()) {
+        await setDoc(doc(db, 'users', user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        })
+      }
+      navigate('/')
+    } catch (error) {
+      toast.error('Could not login with google')
+      console.log(error)
+    }
+  }
 
   return (
     <div className='socialLogin'>
