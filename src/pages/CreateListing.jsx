f71fb79d278b7c43f6023aba1e06f87c9f2a45b9
Adesh Gupta <adesh02092000@gmail.com>
2022-06-13 00:46:15 +0530

    Added initial state for the from data and added the userRef in the state from firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,3 +1,43 @@
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useEffect, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 export default function CreateListing() {
+  const [formData, setFormData] = useState({
+    type: 'rent',
+    name: '',
+    bedrooms: 1,
+    bathrooms: 1,
+    parking: false,
+    furnished: false,
+    address: '',
+    offer: false,
+    regularPrice: 0,
+    discountedPrice: 0,
+    imgUrls: [],
+    latitude: 0,
+    longitude: 0,
+  })
+
+  const auth = getAuth()
+  const navigate = useNavigate()
+  const isMounted = useRef(true)
+
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, user => {
+        if (user) {
+          setFormData({ ...formData, userRef: user.uid })
+        } else {
+          navigate('/sign-in')
+        }
+      })
+    }
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted])
+
   return <div>CreateListing</div>
 }
