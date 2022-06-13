217bb15f5aad170d3128e482ebdd9c731a42f1ae
Adesh Gupta <adesh02092000@gmail.com>
2022-06-13 19:07:52 +0530

    Added UI for the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,18 +1,19 @@
-import { useNavigate, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import { doc, getDoc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
-// import shareIcon from '../assets/svg/shareIcon.svg'
+import shareIcon from '../assets/svg/shareIcon.svg'
 import { useEffect, useState } from 'react'
 import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+import Spinner from '../components/Spinner'
 
 export default function Listing() {
   const [listing, setListing] = useState(null)
   const [loading, setLoading] = useState(true)
-  // const [shareLinkCopied, setShareLinkCopied] = useState(false)
 
   // const navigate = useNavigate()
   const params = useParams()
-  // const auth = getAuth()
+  const auth = getAuth()
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -29,5 +30,68 @@ export default function Listing() {
     fetchListing()
   }, [])
 
-  return <div>Listing</div>
+  if (loading) {
+    return <Spinner />
+  }
+
+  return (
+    <main>
+      <div
+        className='shareIconDiv'
+        onClick={() => {
+          // Copy the link to clipboard
+          navigator.clipboard.writeText(window.location.href)
+          toast.success('Link Copied!')
+        }}
+      >
+        <img src={shareIcon} alt='share' />
+      </div>
+
+      <div className='listingDetails'>
+        <p className='listingName'>
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className='listingLocation'>{listing.location}</p>
+        <p className='listingType'>
+          For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+        </p>
+        {listing.offer && (
+          <p className='discountPrice'>
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className='listingDetailsList'>
+          <li>
+            {listing.bedrooms} {listing.bedrooms > 1 ? 'Bedrooms' : 'Bedroom'}
+          </li>
+          <li>
+            {listing.bathrooms}{' '}
+            {listing.bathrooms > 1 ? 'Bathrooms' : 'Bathroom'}
+          </li>
+          <li>{listing.parking && 'Parking Spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+        <p className='listingLocationTitle'>Location</p>
+        {/* MAP */}
+
+        {/* Add contact info if the listing doesn't belong to the user */}
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+            className='primaryButton'
+          >
+            Contact Landlord
+          </Link>
+        )}
+      </div>
+    </main>
+  )
 }
